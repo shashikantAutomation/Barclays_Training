9dcd8692b72cd53e1ceef0028fc14c381bc5b4fa
shashikantAutomation <shashikantp1@gmail.com>
2019-01-10 03:53:00 -0800

    Just a normal space 2nd

--- src/org/Barclays/Pages/HomePage.java
+++ src/org/Barclays/Pages/HomePage.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class HomePage {
-	WebDriver driver;
+	WebDriver driver; 
 	WebDriverWait wait;
 	@FindBy(linkText=("My Account"))
 	public WebElement myAccLoc;
